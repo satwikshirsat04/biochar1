39594529e283d52a0eecd0bbff90a9b947ebe121
Satwik Shirsat <161075149+satwikshirsat04@users.noreply.github.com>
2025-05-02 17:08:38 +0530

    Update FarmerPhotoSlider.tsx

--- client/src/components/FarmerPhotoSlider.tsx
+++ client/src/components/FarmerPhotoSlider.tsx
@@ -9,23 +9,23 @@ const FarmerPhotoSlider: React.FC = () => {
     },
     {
       id: 2,
-      image: "/images/farmers/f2.jpg"
-    },
-    {
-      id: 3,
-      image: "/images/farmers/f3.jpg"
-    },
-    {
-      id: 4,
       image: "/images/farmers/f4.jpg"
     },
     {
+      id: 3,
+      image: "/images/farmers/f2.jpg"
+    },
+    {
+      id: 4,
+      image: "/images/farmers/f5.jpg"
+    },
+    {
       id: 5,
-      image: "/images/farmers/f5.avif"
+      image: "/images/farmers/f6.jpg"
     },
     {
       id: 6,
-      image: "/images/farmers/f6.webp"
+      image: "/images/farmers/f3.jpg"
     }
   ];
 
