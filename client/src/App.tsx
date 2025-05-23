d7352385371f44f66301eae5bf24022cee4e0713
Satwik Shirsat <161075149+satwikshirsat04@users.noreply.github.com>
2025-05-23 18:51:07 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ScrollRestoration } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Index from "./pages/Index";
@@ -11,6 +12,8 @@ import HowToUse from "./pages/HowToUse";
 import AboutUs from "./pages/AboutUs";
 import NotFound from "./pages/NotFound";
 import Order from "./pages/Order";
+import OrderSuccess from "./pages/OrderSuccess";
+
 
 const queryClient = new QueryClient();
 
@@ -20,11 +23,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollRestoration/>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/how-to-use" element={<HowToUse />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/order" element={<Order />} />
+          <Route path="/order-success" element={<OrderSuccess />} />
+          {/* ADD ALL CUSTOM ROUTES BELOW */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
