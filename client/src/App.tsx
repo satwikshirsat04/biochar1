cacd4c702d551351b48a9c407108e28294242cd2
Satwik Shirsat <161075149+satwikshirsat04@users.noreply.github.com>
2025-05-23 19:03:00 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ScrollRestoration } from "react-router-dom";
+// import { ScrollRestoration } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Index from "./pages/Index";
@@ -23,7 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <ScrollRestoration/>
+        
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/how-to-use" element={<HowToUse />} />
